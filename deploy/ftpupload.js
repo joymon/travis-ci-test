0e9dbfb4a4c94a216871407ce7d90381a4ae5e79
joymon <joymon@gmail.com>
2015-11-07 21:31:20 -0500

    Added loggin of pass length

--- deploy/ftpupload.js
+++ deploy/ftpupload.js
@@ -28,6 +28,7 @@ function uploadToFTP(files) {
 
     console.log("ftp.host =" + ftpConfig.host);
     console.log("ftp.username =" + ftpConfig.username);
+    console.log("ftp.password length =" + ftpConfig.password.length);
     console.log("ftp.localRoot =" + ftpConfig.localRoot);
     console.log("ftp.remoteRoot =" + ftpConfig.remoteRoot);
     console.log("ftp.port =" + ftpConfig.port);
